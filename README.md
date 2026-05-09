b6386967ccb4313cff8507802c31a32ab73867c6
Noel Codes <no3lcodes@gmail.com>
2026-05-09 17:14:47 -0500

    Initializing the project with a README and SQL files for the schema

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Workout Tracker
+This is a personal workout tracker that you can use to track your workouts, you can define any exercises you want to track, define any routines, and use it any time you want to record a workout.
+
+The project has a server in Python.
---
+++ server/data/sql/schema.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    dob DATE NOT NULL
+);
+
+-- IF NOT EXISTS for TYPE is only available in Postgre > v14
+CREATE TYPE IF NOT EXISTS workout_types AS ENUM (
+    'pull',
+    'push',
+    'legs',
+    'cardio',
+    'core'
+);
+
+CREATE TABLE IF NOT EXISTS exercises (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS routines (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    notes TEXT NOT NULL,
+    active BOOL NOT NULL DEFAULT true,
+    workout_type workout_types NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS workouts (
+    id SERIAL PRIMARY KEY,
+    workout_type workout_types NOT NULL,
+    workout_start_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    workout_end_time TIMESTAMPTZ,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    routine_id INTEGER NOT NULL REFERENCES routines(id)
+);
+
+CREATE TABLE IF NOT EXISTS exercise_entries (
+    id SERIAL PRIMARY KEY,
+    workout_id INTEGER NOT NULL REFERENCES workouts(id),
+    exercise_id INTEGER NOT NULL REFERENCES exercises(id)
+);
+
+CREATE TABLE IF NOT EXISTS completed_set_entries (
+    id SERIAL PRIMARY KEY,
+    weight_in_lbs INTEGER DEFAULT 0,
+    rep_count INTEGER DEFAULT 0,
+    duration_in_seconds INTEGER DEFAULT 0,
+    exercise_entry_id INTEGER NOT NULL REFERENCES exercise_entries(id),
+    notes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS routine_exercises (
+    id SERIAL PRIMARY KEY,
+    routine_id INTEGER NOT NULL REFERENCES routines(id),
+    exercise_id INTEGER NOT NULL REFERENCES exercises(id)
+);
+
+CREATE TABLE IF NOT EXISTS planned_sets (
+    id SERIAL PRIMARY KEY,
+    planned_weight INTEGER,
+    planned_reps INTEGER,
+    planned_durations_in_seconds INTEGER,
+    routine_exercise_id  INTEGER NOT NULL REFERENCES routine_exercises(id)
+);
+
+-- Think if we need the routine_exercises, if we get rid of it, the sets can reference the exercises.
